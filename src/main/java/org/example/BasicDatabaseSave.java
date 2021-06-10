1c65d4b3c6398f80501481b31f8b976170c9850f
frb92 <bartek1424@op.pl>
2021-06-10 23:13:09 +0200

    Initial commit

---
+++ src/main/java/org/example/BasicDatabaseSave.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class BasicDatabaseSave {
+    public static void main(String[] args) {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+
+        Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+    }
+}
